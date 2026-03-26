9765feb5f654b5b7cdcf9f1d8b0f42102a615979
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-03-26 16:47:42 -0700

    exercise done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,14 +1,11 @@
 const repeatString = function (str, num) {
-  let newStr = "";
-
-  if (num < 0) {
-    newStr = "ERROR";
-  } else {
-    for (let i = 0; i < num; i++) {
-      newStr += str;
-    }
+  let newStr=""
+  if (num <0) return 'ERROR'
+  for (let i=1 ; i<=num ; i++){
+    newStr +=(str)
+   
   }
-  return newStr;
+  return newStr
 };
 
 // Do not edit below this line
