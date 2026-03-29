ed9dfbd20dcee40bd5267738d773c01a430e3195
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-03-28 19:40:47 -0700

    exercise done and tested

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+ if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0)
+   return "ERROR";
+ if (x>y){
+  [x,y]=[y,x]
+  
+ }
+ let sum=0
+ for (let i=x ; i<=y ; i++){
+  sum+=i
+ }
 
+ 
+  
+return sum
 };
 
 // Do not edit below this line
