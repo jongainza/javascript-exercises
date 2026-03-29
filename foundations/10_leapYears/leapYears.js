bdf78a92dd81d4248284bab791802bbea84ccbbb
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-03-28 20:16:00 -0700

    exercise done and tested

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+let isDivisibleByFour = year%4===0;
+let isDivisibleByHoundred = year%100===0;
+let isDivisibleByFourHundred = year%400===0;
+
+if (isDivisibleByFour&&(!isDivisibleByHoundred||isDivisibleByFourHundred)){
+ return true
+}else{
+ return false
+}
 
 };
 
