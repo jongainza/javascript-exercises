5d7050a18cf3c7badd57fb29051cad26e27f4c02
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-03-30 05:40:05 -0700

    exercise done and tested

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32)*5/9
+  c= Number(c).toFixed(1)
+  return parseFloat(c)
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c *9/5+32
+  f=Number(f).toFixed(1)
+  return parseFloat(f)
+
 };
 
 // Do not edit below this line
