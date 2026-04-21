0a719f0f921762cf9e01be2745fc643d4ed3606d
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-21 05:47:25 -0700

    exercise done and tested

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,curr)=>{return acc+curr},0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc,curr)=>{return acc*curr},1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let result = 1
+ if (n<0){
+    return new Error ('No factorial posible of negative numbers')
+  }else{
+    for (let i = 1; i <= n; i++) {
+      result = result * i;
+    }
+  }
+
+  return result
 	
 };
 
