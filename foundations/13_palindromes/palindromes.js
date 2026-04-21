e84163591a2f70e532ccebdc75bfc93a578306af
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-21 16:18:35 -0700

    exercise done and tested

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+let modifiedString = str.toLowerCase()
+                           .replace(/\s/g, "")
+                           .replace(/\p{P}/gu, "")
+                           .split("");
+for (let i = 0 ; i < modifiedString.length ; i++){
+ 
+ if (modifiedString[i]!==modifiedString[modifiedString.length-1-i]){
+  return false
+ }
+  
+ 
+}
+return true
 
 };
 
