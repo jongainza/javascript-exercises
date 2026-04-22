3d4b876e92e3ab7d01c9862c5881723c7f3a5494
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-22 15:08:03 -0700

    exercise done and tested

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+return  arr.map(book=>book.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
