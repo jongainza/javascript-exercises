c8c8d1c0f70358f1bc4ecb008e00d0bb4f27adf2
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-22 05:35:33 -0700

    exercise done and tested

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+ num = +num
+ let x=2;
+ let secuence = [1,1];
+ if (num===0){
+  return 0
+ }else if(num<0){
+  return 'OOPS'
+ }
+while (x<num){
+let nextNumber = secuence[secuence.length - 1] + secuence[secuence.length - 2];
+secuence.push(nextNumber);
+x++;
+}
+return secuence[secuence.length - 1];
 };
 
 // Do not edit below this line
