32121e6cdffcee7e7b1772e71173b867de4947f8
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-04-27 16:03:49 -0700

    exercise done and tested

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ const oldy = people.reduce((oldest,current)=>{
+ const newDate = new Date().getFullYear();
+ let oldestPerson = (oldest.yearOfDeath || newDate)-oldest.yearOfBirth;
+ let currentPerson = (current.yearOfDeath || newDate)-current.yearOfBirth
+ return oldestPerson>currentPerson?oldest:current
+ 
+},people[0])
+console.log(oldy)
+return oldy
+}
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
